54205fea07c6f0bd48e065b402ddf6c7da1724d5
Sven Günther <sven.guenther@it-agile.de>
2016-08-23 17:44:11 +0200

    entferne Interface Rumpf

--- src/main/java/com/itagile/tddcamp/log/LogReporting.java
+++
@@ -1,7 +0,0 @@
-package com.itagile.tddcamp.log;
-
-import com.itagile.tddcamp.terminkalender.Logger;
-
-public interface LogReporting {
-	void loggerCouldNotLogEntry(Logger logger, String message);
-}
--- src/main/java/com/itagile/tddcamp/terminkalender/Logger.java
+++
@@ -1,5 +0,0 @@
-package com.itagile.tddcamp.terminkalender;
-
-public interface Logger {
-	void neuerTerminEingetragen(Kalender kalender, Termin termin);
-}
